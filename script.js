7980097ac5cc243e5cf5dab9dc16721dccc78747
alejapidaza <alejapidaza30@gmail.com>
2024-10-01 19:11:37 -0500

    v2

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ let diaConMasAlumnos = dias[alumnosPorDia.indexOf(maxAlumnos)];
 alert(`El total de alumnos que vinieron durante la semana es: ${total}`);
 
 alert(`El día con más alumnos fue el ${diaConMasAlumnos} con ${maxAlumnos} alumnos.`);
+
+const resultadosDiv = document.createElement("div");
+resultadosDiv.className = "resultados"; // Clase resultados
+document.body.appendChild(resultadosDiv);
+
+resultadosDiv.innerHTML = `
+    <p>El total de alumnos que vinieron durante la semana es: ${total}</p>
+    <p>El día con más alumnos fue el ${diaConMasAlumnos} con ${maxAlumnos} alumnos.</p>
+`;
+
+// Para activar la animación
+resultadosDiv.classList.add("show");
